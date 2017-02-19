846d6b8b0918d74dd87aee674564eaaa937412c3
Unknown <MOTYSHIZ@gmail.com>
2017-02-18 22:10:20 -0800

    Initial Commit of Unity Files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 10;
+    private Rigidbody rig;
+
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+
+        rig.MovePosition(transform.position + movement);
+	}
+}
