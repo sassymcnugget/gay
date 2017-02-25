4f853039b8bf67695d07422219cddde070c89d0a
Unknown <MOTYSHIZ@gmail.com>
2017-02-25 15:24:30 -0800

    Neku Shadow Scripted and Neku prefab created

---
+++ Assets/Scripts/NekuShadow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NekuShadow : MonoBehaviour {
+    public GameObject shadow;
+    
+	// Update is called once per frame
+	void Update () {
+        shadow.transform.position = new Vector3(transform.position.x,0.01f,transform.position.z);
+	}
+}
