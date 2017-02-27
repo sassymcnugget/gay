c52cb19fc0707e3e58556c2e7168cfb361095d70
Unknown <MOTYSHIZ@gmail.com>
2017-02-26 20:59:05 -0800

    got rid of wow queen and practive
    >:^(

---
+++ Assets/Scripts/MarissaTesting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarissaTesting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.LogError("henlo u stinky debug go eat a internet stripper virtual grill h and d virus STINKY");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i=17; i<87; i++)
+        {
+            Debug.LogError("justin is  huuuuuuge gay homo uguuu");
+        }
+    }
+}
