01a31aeb2e6a867b48c06019128a07b3f3202766
Unknown <MOTYSHIZ@gmail.com>
2017-03-13 10:39:27 -0700

    Marissa's first script!

---
+++ Assets/Scripts/capsuleguyvrity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This is Marissa's first script!
+public class capsuleguyvrity : MonoBehaviour {
+
+    Rigidbody poop;
+
+	// Use this for initialization
+	void Start () {
+        poop = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // If someone left clicks, do...
+		if (Input.GetButtonDown("Fire1"))
+        {
+            poop.useGravity = false;
+            Debug.Log("poopening");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            poop.useGravity = true;
+            Debug.Log("farewell interstellar steedman");
+        }
+	}
+}
